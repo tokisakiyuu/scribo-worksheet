47a5f65e3f55ed886ec330409cd65329d4ab3d6c
tokisakiyuu <tokisakiyuu@qq.com>
2024-01-21 19:17:22 +0800

    disable apollo client request cache

--- lib/graphql-client.ts
+++ lib/graphql-client.ts
@@ -1,13 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const cache = new InMemoryCache();
-const GQLClient = new ApolloClient({
-  cache: cache,
+const link = new HttpLink({
   uri: process.env.ATLASSIAN_GRAPHQL_URI,
   headers: {
     Authorization: `Basic ${process.env.ATLASSIAN_APP_TOKEN}`,
     "X-ExperimentalApi": "softwareBoards,name,JiraIssueSearch",
   },
+  fetch: (input: any, init: any) => {
+    return fetch(input, {
+      ...init,
+      cache: "no-store",
+    });
+  },
+});
+
+const GQLClient = new ApolloClient({
+  cache: cache,
+  link,
 });
 
 export default GQLClient;
