f25f0c752d004960f3d2bb436755b0f9058f5205
tokisakiyuu <tokisakiyuu@qq.com>
2024-01-24 02:05:22 +0800

    add endpoint for send to slack

---
+++ app/api/sendToSlack/route.ts
@@ -0,0 +1,7 @@
+import sendToSlack from "@/actions/sendToSlack";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await sendToSlack();
+  return NextResponse.json({ ok: true });
+}
